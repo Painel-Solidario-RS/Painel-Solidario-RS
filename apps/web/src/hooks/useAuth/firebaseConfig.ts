d61fec6c75c6519b5420082bbea7057db5285fa2
João Victor Perin <joaovperin@live.com>
2024-06-27 20:03:42 -0300

    added missing vite build args to WEB (#61)

--- apps/web/src/hooks/useAuth/firebaseConfig.ts
+++ apps/web/src/hooks/useAuth/firebaseConfig.ts
@@ -1,9 +1,14 @@
-import { initializeApp } from "firebase/app";
+import { FirebaseApp, initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_PUBLIC_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_PUBLIC_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PUBLIC_PROJECT_ID,
+  appId: import.meta.env.VITE_FIREBASE_PUBLIC_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_PUBLIC_MEASUREMENT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_PUBLIC_MESSAGING_SENDER_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
+const app: FirebaseApp = initializeApp(firebaseConfig);
+export { app };
